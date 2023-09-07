35a9bf3896fb549171d566f14f895bf0588d9edf
norah-salim <142896757+norah-salim@users.noreply.github.com>
2023-09-07 14:21:21 +0300

    cw6

--- main.py
+++ main.py
@@ -1,1 +1,37 @@
 # write your code here
+#الجزء الاول
+person = {
+    "name":"NORAH",
+    "age":16,
+    "habbies":["Study"]
+}
+print(person["name"])
+print(person["age"])
+
+#الجزء الثاني
+person["age"] = 17
+person["newproperty"] = "America"
+print("person dictionary:",person)
+print("person dictionary:", len(person))
+
+#الجزء الثالث
+person["habbies"].append("Read")
+print("person dictionary", person)
+
+def check_hobbies(person):
+    if "hobbies" in person:
+        hobbies = person["hobbies"]
+        if len(hobbies) > 3:
+            person("wow you are amazing")
+        return hobbies
+    else:
+        return []
+
+person = {
+    "name": "Norah",
+    "age" : 17,
+    "hobbies":["study,read"]
+}
+
+hobbies = check_hobbies(person)
+print("hobbies")
